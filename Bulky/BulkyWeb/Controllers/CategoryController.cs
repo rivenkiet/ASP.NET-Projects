ade63b21a0ee6af8659ac006c24f75b34a8dbe26
kiet <rivenkiet@gmail.com>
2024-09-29 23:57:47 +0700

    Add CRUD operations

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -24,9 +24,75 @@ namespace BulkyWeb.Controllers
 		[HttpPost]
 		public IActionResult Create(Category obj)
 		{
-			_db.categories.Add(obj);
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "Name mustnot exactly match Display Order");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.categories.Find(id);
+			/*Category? categoryFromDb1 = _db.categories.FirstOrDefault(u => u.Id == id);
+			Category? categoryFromDb2 = _db.categories.Where(u => u.Id == id).FirstOrDefault();*/
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.categories.Find(id);
+			/*Category? categoryFromDb1 = _db.categories.FirstOrDefault(u => u.Id == id);
+			Category? categoryFromDb2 = _db.categories.Where(u => u.Id == id).FirstOrDefault();*/
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? obj = _db.categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.categories.Remove(obj);
 			_db.SaveChanges();
 			return RedirectToAction("Index");
 		}
+
 	}
 }
